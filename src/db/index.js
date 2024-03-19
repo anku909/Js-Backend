873e284e7a7f9f4bca77ed6322d432ce73add177
anku <ankuofficial909@gmail.com>
2024-03-19 23:29:20 +0530

    mongoDB setup

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constansts.js";
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nMongoDB connected !! DB Host : ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.log("MongoDB conncetion error : ", err);
+    process.exit(1);
+  }
+};
